a5c5f6700e7352d78660c52b5aa93eafbbdc7217
RafikiJules <ihimbazwerafikijules@gmail.com>
2025-12-06 15:52:06 +0200

    Fix code formatting for /about route

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 const http = require('http');
 
+const hostname = '127.0.0.1';
+const port = 3000;
+
 const server = http.createServer((req, res) => {
-    if (req.url === '/about') {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('About Page\n');
-    } else {
-        res.statusCode = 200;
-        res.setHeader('Content-Type', 'text/plain');
-        res.end('Hello, World!\n');
-    }
+  if (req.url === '/') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Hello World\n');
+  } else if (req.url === '/about') {
+    res.statusCode = 200;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('About Page\n');
+  } else {
+    res.statusCode = 404;
+    res.setHeader('Content-Type', 'text/plain');
+    res.end('Page Not Found\n');
+  }
 });
 
-server.listen(3000, () => {
-    console.log('Server running at http://localhost:3000/');
+server.listen(port, hostname, () => {
+  console.log(`Server running at http://${hostname}:${port}/`);
 });
